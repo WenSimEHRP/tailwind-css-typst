8db5a2e79c8071e48b513b137af5061b76ba3562
Jeremy Gao <wensimehrp@gmail.com>
2026-05-11 15:11:18 -0700

    feat: add prose

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use cbor4ii::serde::from_slice;
 use encre_css::Config;
+use encre_css_typography::Prose;
 use typst_wasm_protocol::wasm_export;
 
 #[wasm_export]
 pub fn generate(content: &[u8], config_bytes: &[u8]) -> Vec<u8> {
     let s = std::str::from_utf8(content).unwrap();
-    let config = if config_bytes.is_empty() {
+    let mut config = if config_bytes.is_empty() {
         Config::default()
     } else {
         from_slice(config_bytes).unwrap()
     };
+    config.register_plugin("prose", &Prose);
     encre_css::generate(s.split_whitespace(), &config).into_bytes()
 }
