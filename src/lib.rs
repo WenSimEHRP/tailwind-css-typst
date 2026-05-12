8e7770d561a4183dbf189ae8331df2a8f3c0fd83
Jeremy Gao <wensimehrp@gmail.com>
2026-05-11 23:29:30 -0700

    fix: typography plugin registration

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use cbor4ii::serde::from_slice;
 use encre_css::Config;
-use encre_css_typography::Prose;
 use typst_wasm_protocol::wasm_export;
 
 #[wasm_export]
@@ -11,6 +10,6 @@ pub fn generate(content: &[u8], config_bytes: &[u8]) -> Vec<u8> {
     } else {
         from_slice(config_bytes).unwrap()
     };
-    config.register_plugin("prose", &Prose);
+    encre_css_typography::register(&mut config);
     encre_css::generate(s.split_whitespace(), &config).into_bytes()
 }
